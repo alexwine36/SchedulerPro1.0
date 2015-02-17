cdb4545eff71766eb53fdf98027f98f8c9b1b28b
alexanderwine <alexanderwine@192.168.0.3>
2015-02-17 16:12:17 -0700

    iCal Array created based on new events

--- update_ics.php
+++ update_ics.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * Copyright (C) 2015 alexanderwine
  *
  * This program is free software: you can redistribute it and/or modify
@@ -18,8 +18,10 @@
  */
 
 
-$page_title = 'Update .ics';
-include_once 'header.php';
+include("includes/connection.php");
+include("includes/icalendar.php");
+//$records = mysql_num_rows(mysql_query("select * from  events"));
+//if ($_POST['stage'] == 1) {
 
 $rooms = array("http://www.icalx.com/public/mmmcabq/Arts__Crafts_Room_3_Calendar.ics",
     "http://www.icalx.com/public/mmmcabq/Social_Hall_East_Calendar.ics",
@@ -43,10 +45,111 @@ for ($x = 0; $x < $arrayLength; $x++) {
     echo "File ";
     echo $y;
     echo "<br><br><br>";
-    echo readfile($rooms[$x]);
+    //echo $_FILES[$rooms['x']]['size'];
+    //echo readfile($rooms[$x]);
+    $icsFile = fopen($rooms[$x], 'r') or die("Unable to open file!");
+    //fread($icsFile, filesize($icsFile)) or die("Unable to read file!");
+
+    while (!feof($icsFile)) {
+        $dataString = fgets($icsFile) . "<br>";
+        //echo $dataString;
+        $ds = explode(":", $dataString);
+        if (stristr($dataString, "begin:vevent")) {
+            //echo stristr($dataString, 'begin:vevent');
+            //print_r(explode(":", $dataString));
+            echo $dataString;
+            $event = array();
+        }
+        if (stristr($dataString, "end:vevent")) {
+            foreach ($event as $t => $t_value) {
+                echo '<br>';
+                echo 'Key: '
+                . $t . ", Value: "
+                . $t_value;
+                echo '<br>';
+            }
+            echo $dataString;
+            unset($event);
+        }
+        switch ($ds['0']) {
+            case 'DESCRIPTION':
+                echo $ds['1'];
+                $event['description'] = $ds['1'];
+                break;
+            case 'LAST-MODIFIED':
+                echo $ds['1'];
+                $event['last-modified'] = $ds['1'];
+                break;
+            case 'DTEND':
+                echo $ds['1'];
+                $event['dtend'] = $ds['1'];
+                break;
+            case 'DTSTART':
+                echo $ds['1'];
+                $event['dtstart'] = $ds['1'];
+                break;
+            case 'SUMMARY;LANGUAGE=en-us':
+                echo $ds['1'];
+                $event['summary'] = $ds['1'];
+                break;
+            case 'CATEGORIES':
+                echo $ds['1'];
+                $event['categories'] = $ds['1'];
+                break;
+            case 'UID':
+                echo $ds['1'];
+                $event['uid'] = $ds['1'];
+                break;
+        }
+    }
+    fclose($icsFile);
 }
+/*
+  $ical = new iCalendar();
+  $filename = $_FILES['file1']['tmp_name'];
+  $ical->parse("$filename");
+  $ical_data = $ical->get_all_data();
+
+  $timezone = "{$ical_data['VCALENDAR']['X-WR-TIMEZONE']}";
+  if (function_exists('date_default_timezone_set'))
+  date_default_timezone_set($timezone);
+  $strsql1 = "Insert into  events(ID,StartDate,StartTime,EndDate,EndTime,Title,Location,Description) values  ";
+  if (!empty($ical_data['VEVENT'])) {
+  foreach ($ical_data['VEVENT'] as $key => $data) {
+
+  //get StartDate And StartTime
+  $start_dttimearr = explode('T', $data['DTSTART']);
+  $StartDate = $start_dttimearr[0];
+  $startTime = $start_dttimearr[1];
+
+  //get EndDate And EndTime
+  $end_dttimearr = explode('T', $data['DTEND']);
+  $EndDate = $end_dttimearr[0];
+  $EndTime = $end_dttimearr[1];
+
+  $strsql1.="('" . $data['UID'] . "','" . $StartDate . "','" . $startTime . "','" . $EndDate . "','" . $EndTime . "','" . mysql_real_escape_string($data['SUMMARY;LANGUAGE=en-us']) . "','" . mysql_real_escape_string($data['LOCATION']) . "','" . mysql_real_escape_string($data['DESCRIPTION']) . "')";
+  $strsql1.=",";
+  }
+  $strsql1 = rtrim($strsql1, ',');
+
+  mysql_query($strsql1);
+  }
+
+  header('Location:index.php');
+
+
+  if ($_GET['stage'] == "empty") {
+  mysql_query(" TRUNCATE events");
+  header('Location:index.php');
+  }
+ * 
+ */
+
+$page_title = 'Update .ics';
+include_once 'header.php';
 ?>
 
 <?php
+
 include_once 'footer.php';
 ?>
